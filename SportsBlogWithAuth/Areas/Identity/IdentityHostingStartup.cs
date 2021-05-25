5149439c1c538361b08eca8413a310b9601e89ff
Hoodys352 <przemekptasznikmufc@gmail.com>
2021-05-25 18:32:12 +0200

    Polish description in signin and register

---
+++ SportsBlogWithAuth/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SportsBlogWithAuth.Data;
+
+[assembly: HostingStartup(typeof(SportsBlogWithAuth.Areas.Identity.IdentityHostingStartup))]
+namespace SportsBlogWithAuth.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
